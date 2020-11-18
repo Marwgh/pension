5cf50f1fc3916db562bbec97309e515d310dfb04
ArraysStartAtZero <70196769+ArraysStartAtZero@users.noreply.github.com>
2020-11-18 15:29:04 +0100

    Update style.css

---
+++ style.css
@@ -0,0 +1,44 @@
+body{
+    background-color:#ECEFF2;
+    margin:0;
+}
+
+.logo_icon{
+background-color: white;
+margin-top: 3%;
+height: 96px;
+}
+
+.navbar{
+    background-color: white;
+    margin-top: 1%;
+    height: 46px;
+    max-width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+   
+    }
+.navbar a{
+margin-top: 12px;
+text-decoration: none;
+}
+
+.video{
+    display: flex;
+    justify-content: center;
+    margin-top: 3%;
+}
+
+.video2{
+
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 2%; 
+    margin-left: 20%;
+    margin-right: 20%;
+}
+
+.video2 img{
+    border-radius: 6%;
+    height:119px;
+}
