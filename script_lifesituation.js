57627325ddabb0143f7076f4fd8a53a57f69224c
Marwgh <70144158+Marwgh@users.noreply.github.com>
2020-11-18 15:28:52 +0100

    fecting-js/template

---
+++ script_lifesituation.js
@@ -0,0 +1,34 @@
+/*fetching the data*/
+function init() {
+    fetch("http://allofme.dk/ap/wp-json/wp/v2/lifesituation").then(r => r.json()).then(
+        function (data) {
+            createSections(data);
+            console.log(data)
+        }
+    )
+}
+init();
+
+
+function createSections(categories) {
+    categories.forEach(showproduct);
+}
+
+function showproduct(product) {
+    const templ = document.querySelector("#Template").content;
+    const copy = templ.cloneNode(true);
+
+    const h1 = copy.querySelector("h1");
+    h1.textContent = product.title_of_description;
+
+    copy.querySelector(".text").innerHTML = product.content.rendered;
+
+    /*
+    const img = copy.querySelector(".productImg");
+    img.setAttribute('src', product.location_image.guid);
+    */
+    document.querySelector("main").appendChild(copy);
+
+
+
+}
